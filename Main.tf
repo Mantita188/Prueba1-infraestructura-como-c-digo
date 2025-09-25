87db4f5f6a53f5aebc9378904de44b19622a0603
Mantita188 <conita551@gmail.com>
2025-09-25 15:34:15 -0300

    Se agrega archivo de visual studio.
    Archivo contiene lo trabajado en la primera prueba de la implementación de terraform.
    Se crea una vpc con sus caracteristicas, se realiza la implemetacion de una instancia, bucket s3 y base de datos :D.

---
+++ Main.tf
@@ -0,0 +1,198 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name 
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+
+  tags = {
+    Name      = each.key 
+    Terraform = "true"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = each.key 
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name      = "tabla-de-enrutamiento-publica-de-conny" 
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "tabla-de-enrutamiento-privada-de-conny"
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.public_subnets]
+  route_table_id = aws_route_table.public_route_table.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "internet-gateway-de-conny"
+  }
+}
+
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "eip-para-nat-gateway-de-conny"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["subred-publica-1a"].id
+
+  tags = {
+    Name = "nat-gateway-de-conny"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "SG-de-prueba-conny"
+  description = "Allow HTTP and SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG-de-prueba-conny"
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["subred-publica-1a"].id
+
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<title>Servidor web de Conny desplegado con Terraform</title>" > /var/www/html/index.html
+              echo "<h1>Creado por: Constanza Catalina Retamal Muñoz</h1>" >> /var/www/html/index.html
+              echo "<h2>Asignatura: Infraestructura como codígo </h2>" >> /var/www/html/index.html
+              echo "<h3>Docente: Jorge Ramírez </h3>" >> /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "servidor-web-prueba1-terraform-de-conny"
+  }
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the web server instance."
+  value       = aws_instance.web_server.public_ip
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "cony-my-tf-test-bucket"
+
+  tags = {
+    Name        = "Bucket_de_conny"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
